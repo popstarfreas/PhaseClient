449e00e5977941a14e17cbf0175ea4f46f50bd86
Aaren Cooper <popstarfreas2@gmail.com>
2017-02-01 17:57:04 +0000

    Start edit queue implementation

--- app/app.ts
+++ app/app.ts
@@ -1,15 +1,22 @@
 import * as $ from 'jquery';
 import Client from 'phasemobile/client';
+import PageModel from 'phasemobile/pagemodel';
+import Network from 'phase/network';
 
 $(() => {
+    console.log("Init");
     // Init tooltips
     $(function() {
         $('[data-toggle="tooltip"]').tooltip();
     });
 
-    let client = new Client({
+    let client = new Client();
+    let network = new Network(client, {
         protocol: 'https',
         host: 't.dark-gaming.com:3001',
         secure: true
     });
+    let pageModel = new PageModel(client);
+
+    client.setup(network, pageModel);
 });
