d99c7a81bf53847327372ee5c7324f5e651cb2e3
Aaren Cooper <popstarfreas2@gmail.com>
2017-02-06 22:48:21 +0000

    Complete impementation of configuration options

--- app/app.ts
+++ app/app.ts
@@ -1,16 +1,15 @@
 import * as $ from 'jquery';
-import Client from 'phase/client';
-import PageModel from 'phase/pagemodel';
+import Client from 'phasemobile/client';
+import PageModel from 'phasemobile/pagemodel';
 import Network from 'phase/network';
 
 $(() => {
-    console.log("Init");
-    // Init tooltips
-    $(function() {
-        $('[data-toggle="tooltip"]').tooltip();
+    let client = new Client({
+        combineMessages: true,
+        shrinkJoinLeaveMessages: true,
+        useAndDisplayJoinLeaveMessages: false
     });
 
-    let client = new Client();
     let network = new Network(client, {
         protocol: 'https',
         host: 't.dark-gaming.com:3001',
