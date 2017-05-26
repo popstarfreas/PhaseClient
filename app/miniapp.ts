f1891fa93d8af60c280718988fa3a4d0145db2fe
Aaren Cooper <popstarfreas2@gmail.com>
2017-05-26 19:34:41 +0100

    New mini version

---
+++ app/miniapp.ts
@@ -0,0 +1,22 @@
+import * as $ from "jquery";
+import Network from "phaseclient/network";
+import Client from "phasemini/client";
+import PageModel from "phasemini/pagemodel";
+
+$(() => {
+    const client = new Client({
+        combineMessages: true,
+        shrinkJoinLeaveMessages: true,
+        useAndDisplayJoinLeaveMessages: false
+    });
+
+    const network = new Network(client, {
+        protocol: "https",
+        host: "t.dark-gaming.com:3001",
+        secure: true
+    });
+    const pageModel = new PageModel(client);
+
+    client.setupNetwork(network);
+    client.setupPageModel(pageModel);
+});
